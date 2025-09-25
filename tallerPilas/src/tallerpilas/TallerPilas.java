0f02d03615d1649ff41e5a6a296f0e1cae7759dc
handelenriquez <handell1210@gmail.com>
2025-09-24 21:18:22 -0600

    Este es el primer commit del proyecto

---
+++ tallerPilas/src/tallerpilas/TallerPilas.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tallerpilas;
+
+/**
+ *
+ * @author handelenriquez
+ */
+public class TallerPilas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("aqui va el menu de main");
+    }
+    
+}
