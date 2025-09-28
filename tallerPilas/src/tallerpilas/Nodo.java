6fce09d7a6666bd5383f62edca17fea106f8c9b7
Stef <gomessteff3@gmail.com>
2025-09-27 20:54:58 -0600

    Feat: agregar clase pila con apilar, desapilar y mostrarHistorial + Nodo

---
+++ tallerPilas/src/tallerpilas/Nodo.java
@@ -0,0 +1,35 @@
+
+package tallerpilas;
+
+/**
+ *
+ * @author jomas
+ */
+public class Nodo {
+    private Pagina dato;
+    private Nodo sig;
+
+    public Nodo(Pagina dato) {
+        this.dato = dato;
+        this.sig = null;
+    }
+
+    public Pagina getDato() {
+        return dato;
+    }
+
+    
+    public Nodo getSig() {
+        return sig;
+    }
+
+    public void setSig(Nodo sig) {
+        this.sig = sig;
+    }
+    
+    
+    
+    
+    
+    
+}
